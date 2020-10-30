556e4a5a70f7e0d9b0290609e02f9727cbeab0b5
Sajjad Ahmad <jogimar14@gmail.com>
2020-10-30 15:29:44 +0500

    add update action and change edit template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,14 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
   private
 
   def article_params
